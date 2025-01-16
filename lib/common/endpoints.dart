75520eb530dbd2cffb207fd8b437de2c87efd2c7
Piotr Ciuba <ciuba.piotr@gmail.com>
2025-01-16 21:44:14 +0100

    F1 endpoints added

---
+++ lib/common/endpoints.dart
@@ -0,0 +1,12 @@
+class Endpoints {
+  String get _f1BaseUrl => 'http://api.jolpi.ca/ergast/f1';
+
+  String get f1Races => '$_f1BaseUrl/races';
+  String get f1Sprint => '$_f1BaseUrl/sprint';
+  String get f1Status => '$_f1BaseUrl/status';
+  String get f1Seasons => '$_f1BaseUrl/seasons';
+  String get f1Drivers => '$_f1BaseUrl/drivers';
+  String get f1Results => '$_f1BaseUrl/results';
+  String get f1Circuits => '$_f1BaseUrl/circuits';
+  String get f1Constructors => '$_f1BaseUrl/constructors';
+}
