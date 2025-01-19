d6ab8017750b12d2c47e86f8c0916033511a530a
Piotr Ciuba <ciuba.piotr@gmail.com>
2025-01-20 00:31:22 +0100

    Logic for fetching races prepared

--- lib/core/blocs/f1/f1_bloc.dart
+++ lib/core/blocs/f1/f1_bloc.dart
@@ -1,4 +1,7 @@
 import 'package:f1_mobile_frontend_demo/core/repositories/f1_repository.dart';
+import 'package:f1_mobile_frontend_demo/models/mr_data/mr_data.dart';
+import 'package:f1_mobile_frontend_demo/models/race/race.dart';
+import 'package:f1_mobile_frontend_demo/models/race_table/race_table.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -9,8 +12,76 @@ class F1Bloc extends Bloc<F1Event, F1State> {
   F1Bloc({
     required this.f1Repository,
   }) : super(F1Initial()) {
-    on<F1Event>((event, emit) {});
+    on<FetchF1RacesEvent>((event, emit) => _fetchRaces(event, emit));
   }
 
   final F1Repository f1Repository;
+
+  Future<void> _fetchRaces(
+    F1Event event,
+    Emitter<F1State> emit,
+  ) async {
+    try {
+      emit(
+        F1FetchingInProgress(
+          mrData: state.mrData,
+          pageIndex: state.pageIndex,
+          pageSize: state.pageSize,
+        ),
+      );
+
+      List<Race>? responseList = await f1Repository.getF1Races(
+        pageLimit: event.pageLimit,
+      );
+      late MrData updatedMrData;
+
+      if (state.mrData == null) {
+        updatedMrData = MrData(
+          raceTable: RaceTable(races: responseList),
+        );
+      } else if (state.mrData?.raceTable?.races == null ||
+          state.mrData!.raceTable!.races!.isEmpty) {
+        updatedMrData = state.mrData!.copyWith(
+          raceTable: RaceTable(races: responseList),
+        );
+      } else {
+        List<Race>? updatedRaceList =
+            (state.mrData!.raceTable!.races! + responseList!).toSet().toList();
+        updatedMrData = state.mrData!.copyWith(
+          raceTable: RaceTable(races: updatedRaceList),
+        );
+      }
+
+      emit(
+        F1StateChanged(
+          mrData: updatedMrData,
+          pageSize: state.pageSize,
+          pageIndex: _adjustPageIndex(
+            updatedMrData.raceTable!.races!.length,
+            state.pageSize,
+            state.pageIndex,
+          ),
+        ),
+      );
+    } catch (e) {
+      emit(
+        F1FetchingFailed(
+          mrData: state.mrData,
+          pageIndex: state.pageIndex,
+          pageSize: state.pageSize,
+          error: e.toString(),
+        ),
+      );
+    }
+  }
+
+  int _adjustPageIndex(int length, int pageSize, int currentPageIndex) {
+    int totalPages = (length / pageSize).ceil();
+
+    if (currentPageIndex >= totalPages) {
+      return totalPages - 1;
+    } else {
+      return currentPageIndex;
+    }
+  }
 }
