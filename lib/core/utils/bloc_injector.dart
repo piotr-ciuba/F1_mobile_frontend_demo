354ca8b140f02f1af97228252ee4b9e292c76e2e
Piotr Ciuba <ciuba.piotr@gmail.com>
2025-01-19 21:06:10 +0100

    Bloc injector created

---
+++ lib/core/utils/bloc_injector.dart
@@ -0,0 +1,42 @@
+import 'package:f1_mobile_frontend_demo/core/blocs/f1/f1_bloc.dart';
+import 'package:f1_mobile_frontend_demo/core/blocs/navigation_bar/navigation_bar_bloc.dart';
+import 'package:f1_mobile_frontend_demo/core/repositories/f1_repository.dart';
+import 'package:f1_mobile_frontend_demo/core/services/f1_service.dart';
+import 'package:f1_mobile_frontend_demo/core/utils/dependency_injection/injector.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BlocInjector extends StatelessWidget {
+  BlocInjector({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  F1Repository get _f1Repository => serviceLocator.get<F1Repository>(
+        param1: serviceLocator.get<F1Service>(),
+      );
+
+  F1Bloc get _f1Bloc => F1Bloc(
+        f1Repository: _f1Repository,
+      );
+
+  final NavigationBarBloc _navigationBarBloc = NavigationBarBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider.value(value: _f1Repository),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider.value(value: _f1Bloc),
+          BlocProvider.value(value: _navigationBarBloc),
+        ],
+        child: child,
+      ),
+    );
+  }
+}
