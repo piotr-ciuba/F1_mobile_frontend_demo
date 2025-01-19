065bbb81c7ecdf991febdbcb182275e0c944d3c2
Piotr Ciuba <ciuba.piotr@gmail.com>
2025-01-19 22:48:19 +0100

    Assets added

---
+++ lib/common/asset_paths.dart
@@ -0,0 +1,27 @@
+mixin AssetPaths {
+  // Images
+  static const String alonso = 'assets/images/jpg/alonso.jpg';
+  static const String hamilton = 'assets/images/jpg/hamilton.jpg';
+  static const String kubica = 'assets/images/jpg/kubica.jpg';
+  static const String verstappen = 'assets/images/jpg/verstappen.jpg';
+  static const String leclerc = 'assets/images/jpg/leclerc.jpg';
+  static const String perez = 'assets/images/jpg/perez.jpg';
+  static const String russell = 'assets/images/jpg/russell.jpg';
+
+  // SVGs
+  static const String f1Logo = 'assets/images/svg/f1_logo.svg';
+
+  static const List<String> jpgImages = [
+    alonso,
+    hamilton,
+    kubica,
+    verstappen,
+    leclerc,
+    perez,
+    russell,
+  ];
+
+  static const List<String> svgImages = [
+    f1Logo,
+  ];
+}
