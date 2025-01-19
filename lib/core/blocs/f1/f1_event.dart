8f14c2b6901c899aa20f6c7d0400c863daf72617
Piotr Ciuba <ciuba.piotr@gmail.com>
2025-01-20 00:30:21 +0100

    States and events prepared for F1 bloc

--- lib/core/blocs/f1/f1_event.dart
+++ lib/core/blocs/f1/f1_event.dart
@@ -1,8 +1,60 @@
 part of 'f1_bloc.dart';
 
 sealed class F1Event extends Equatable {
-  const F1Event();
+  const F1Event({
+    this.pageLimit = 10,
+  });
+
+  final int pageLimit;
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [pageLimit];
+}
+
+class FetchF1RacesEvent extends F1Event {
+  const FetchF1RacesEvent({
+    super.pageLimit,
+  });
+}
+
+class FetchF1StatusEvent extends F1Event {
+  const FetchF1StatusEvent({
+    super.pageLimit,
+  });
+}
+
+class FetchF1SeasonsEvent extends F1Event {
+  const FetchF1SeasonsEvent({
+    super.pageLimit,
+  });
+}
+
+class FetchF1DriversEvent extends F1Event {
+  const FetchF1DriversEvent({
+    super.pageLimit,
+  });
+}
+
+class FetchF1ResultsEvent extends F1Event {
+  const FetchF1ResultsEvent({
+    super.pageLimit,
+  });
+}
+
+class FetchF1CircuitsEvent extends F1Event {
+  const FetchF1CircuitsEvent({
+    super.pageLimit,
+  });
+}
+
+class FetchF1ConstructorsEvent extends F1Event {
+  const FetchF1ConstructorsEvent({
+    super.pageLimit,
+  });
+}
+
+class FetchF1ClearDataEvent extends F1Event {
+  const FetchF1ClearDataEvent({
+    super.pageLimit,
+  });
 }
--- lib/core/blocs/f1/f1_state.dart
+++ lib/core/blocs/f1/f1_state.dart
@@ -1,10 +1,47 @@
 part of 'f1_bloc.dart';
 
 sealed class F1State extends Equatable {
-  const F1State();
+  const F1State({
+    this.mrData,
+    this.pageIndex = 0,
+    this.pageSize = 10,
+  });
+
+  final MrData? mrData;
+  final int pageIndex;
+  final int pageSize;
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [mrData, pageIndex, pageSize];
 }
 
-final class F1Initial extends F1State {}
+final class F1Initial extends F1State {
+  const F1Initial();
+}
+
+final class F1FetchingInProgress extends F1State {
+  const F1FetchingInProgress({
+    required super.mrData,
+    required super.pageIndex,
+    required super.pageSize,
+  });
+}
+
+final class F1FetchingFailed extends F1State {
+  const F1FetchingFailed({
+    required super.mrData,
+    required super.pageIndex,
+    required super.pageSize,
+    required this.error,
+  });
+
+  final String error;
+}
+
+final class F1StateChanged extends F1State {
+  const F1StateChanged({
+    required super.mrData,
+    required super.pageIndex,
+    required super.pageSize,
+  });
+}
