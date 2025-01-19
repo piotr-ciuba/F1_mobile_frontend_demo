b85adc6cf49dd8a08cfe3a679a0e1c6d4d376d3f
Piotr Ciuba <ciuba.piotr@gmail.com>
2025-01-19 21:21:57 +0100

    App routing paths created

---
+++ lib/common/routes.dart
@@ -0,0 +1,32 @@
+class Routes {
+  static const String homeRoute = '/';
+  static const String driverRoute = '/driver';
+  static const String circuitRoute = '/circuit';
+  static const String constructorRoute = '/constructor';
+  static const String seasonRoute = '/season';
+  static const String raceRoute = '/race';
+  static const String raceResultRoute = '/result';
+  static const String statusRoute = '/status';
+  static const String dashboardRoute = '/dashboard';
+  static const String errorRoute = '/error';
+  static const String noInternetRoute = '/noInternet';
+
+  // #auth
+  static const String welcomeRoute = '/welcome';
+  static const String registerRoute = '/register';
+  static const String loginRoute = '/login';
+  static const String forgotPasswordRoute = '/forgotPassword';
+  static const String verificationRoute = '/verification';
+  static const String setNewPasswordRoute = '/setNewPassword';
+  static const String passwordChangedRoute = '/passwordChanged';
+
+  // #profile
+  static const String profileRoute = '/profile';
+  static const String profileSettingsRoute = '/profile/settings';
+  static const String profileEditRoute = '/profile/edit';
+  static const String profileChangePasswordRoute = '/profile/changePassword';
+  static const String profileChangeEmailRoute = '/profile/changeEmail';
+  static const String profileChangePhoneRoute = '/profile/changePhone';
+  static const String profileChangeLanguageRoute = '/profile/changeLanguage';
+  static const String profileChangeThemeRoute = '/profile/changeTheme';
+}
